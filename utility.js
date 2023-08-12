588c197e39f2e40a1171e6d629d525438f769d5f
Michael Pace <michael.g.pace@gmail.com>
2023-08-12 18:03:06 -0500

    added and filled utilityjs to demo12 specs

---
+++ utility.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pickup: 'pickup',
+  pickedUp: 'pickedup', //watch capitalization
+  inTransit: 'inTransit',
+  delivered: 'delivered',
+  announcement: 'announcement',
+  ready: 'ready',
+};
